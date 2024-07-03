adaf458af619a9d87b02bbd0bbd1acdaed86aa9b
Petr Řehoř <rx@rx.cz>
2024-07-03 22:02:20 +0200

    feat(renovate): pin Docker images to digest

--- apps/alpine/ci/latest.sh
+++ apps/alpine/ci/latest.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-set -e
-version=$(
-    curl -s --fail -H "Content-Type: application/json" 'https://hub.docker.com/v2/repositories/library/alpine/tags/?page_size=1000' |
-    jq -r '.results|.[]|.name' |
-    grep -P '^\d+\.\d+\.\d+$' |
-    sort -V -r |
-    head -1
-)
+# renovate: datasource=docker depName=docker.io/library/alpine
+version="3.20"
 printf "%s" "${version}"
